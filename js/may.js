55bab8dda76ea2de89eb320ed3ad07a1ff4120d7
nguyenvannghia <17130136@st.hcmuaf.edu.vn>
2019-10-01 19:40:45 +0700

    a

---
+++ js/may.js
@@ -0,0 +1,22 @@
+function btnplus() {
+    document.maytinh.manhinh.value="+";
+    document.maytinh.manhinh.style.textAlign = "right";
+
+}
+function btnsub() {
+    document.maytinh.manhinh.value+="-";
+    document.maytinh.manhinh.style.textAlign="right";
+}
+function btnclear() {
+    document.maytinh.manhinh.value="";
+    document.maytinh.manhinh.style.textAlign="right";
+}
+function btnnhan() {
+    document.maytinh.manhinh.value=" *";
+    document.maytinh.manhinh.style.textAlign="right";
+
+}
+function btndiv() {
+    document.maytinh.manhinh.value = "";
+    document.maytinh.manhinh.style.textAlign = "right";
+}
